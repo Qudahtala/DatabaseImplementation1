edad320c775f30106b11d03f14173a4a09dedb92
Tala <talaqudah86@gmail.com>
2023-12-03 19:11:57 +0300

    commit

--- src/main/java/com/example/PostingWebsite/post/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PostingWebsite.post;
-
-import com.example.PostingWebsite.post.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Posts, Long> {
-    // query methods later
-}
-
--- src/main/java/com/example/PostingWebsite/relastionship/RelationshipRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.PostingWebsite.relastionship;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RelationshipRepository extends JpaRepository<Relationship, Long> {
-}
